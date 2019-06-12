c73674015c4a63a489682d96dcd852213f8e1d8c
Maria Jose <mariajosenc89@gmail.com>
2019-06-12 13:46:41 -0500

    data is seeded with mongo db

--- db/mongo.js
+++ db/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const faker = require('faker');
-mongoose.connect('mongodb://localhost:27017/amazincdc');
 
+mongoose.connect('mongodb://localhost:27017/amazincdc', { useNewUrlParser: true });
 
 let schema = mongoose.Schema({
     id: Number,
@@ -10,8 +10,7 @@ let schema = mongoose.Schema({
     revBody: String,
     revDate: Date,
     stars: Number,
-})
-
+});
 
 const createFakeReviews = (id) => ({
     id: id,
@@ -19,20 +18,42 @@ const createFakeReviews = (id) => ({
     revName: faker.name.findName(),
     revBody_id: faker.lorem.paragraph(),
     revDate: faker.date.past(),
-    stars: Math.floor(Math.random() * 5)
+    stars: Math.floor(Math.random() * 6)
 });
 
-const Review = mongoose.model('Reviews', schema);
+const Review = mongoose.model('reviews', schema);
 
-const createReviewMongo = () => {
-    const desiredFakeReviews = 10;
+// const records = [
+//     {id: 'Bob',  revTitle: 'French, English', revName:},
+//     {name: 'Mary', lang: 'English'}
+// ];
+
+const createReviewMongo = async () => {
+    let idNumber = 9000001;
+    
+    const desiredFakeReviews = 1000000;
+
     for(let i = 1; i <= desiredFakeReviews; i++) {
-        Review.insertOne(createFakeReviews(), (err) => {
-          if(err) {
-            console.log('error inserting data', err);
-          } 
-        })
+      await createMongoosePromise(idNumber);
+      idNumber = idNumber + 1;    
+      
+      if(i === desiredFakeReviews) {
+        mongoose.connection.close();
+      }
     }
+}
+
+let createMongoosePromise = (id) => {
+  return new Promise ((resolve, reject) => {
+    Review.create(createFakeReviews(id), (err) => {
+
+      if(err) {
+        reject(err);
+      } else {
+        resolve('data is inserted')
+      }
+    });
+  });
 }
 
 
@@ -42,3 +63,15 @@ const createReviewMongo = () => {
 createReviewMongo();
 
 
+
+
+// Review.create(createFakeReviews(idNumber), (err) => {
+//     if(err) {
+//       console.log('error inserting data', err);
+//     } 
+
+//     if(i === desiredFakeReviews) {
+//       mongoose.connection.close();
+//     }
+
+// })
